360d30161fdf7331a94cd387913febc1d2e56d6c
manzyukan <200335q@ugs.kochi-tech.ac.jp>
2020-07-25 12:00:57 +0900

    update test_class.py and clear move

--- input_sensor/test_class.py
+++ input_sensor/test_class.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 import datetime
+import random
 
-
-class Sensor ():
+class Sensor():
 
     # GPIOのPIN
     MagPIN = 14
@@ -12,8 +12,8 @@ class Sensor ():
     YLED = 22
     GLED = 27
     #基準値
-    hourTime = 3600
-    min5Time = 300
+    hourTime = 60
+    min5Time = 10
     ppmOk = 800
     ppmNo = 1000
     #Co2PIN = 
@@ -21,13 +21,13 @@ class Sensor ():
     #セットアップ
     def __init__(self):
         # GPIOのセットアップ
-        self.GPIO.setwarnings(False)
-        self.GPIO.setmode(GPIO.BCM)
-        self.GPIO.setup(MagPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        self.GPIO.setup(LED,GPIO.OUT,initial=GPIO.LOW)
-        self.GPIO.setup(RLED,GPIO.OUT,initial=GPIO.LOW)
-        self.GPIO.setup(YLED,GPIO.OUT,initial=GPIO.LOW)
-        self.GPIO.setup(GLED,GPIO.OUT,initial=GPIO.LOW)
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.MagPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.LED,GPIO.OUT,initial=GPIO.LOW)
+        GPIO.setup(self.RLED,GPIO.OUT,initial=GPIO.LOW)
+        GPIO.setup(self.YLED,GPIO.OUT,initial=GPIO.LOW)
+        GPIO.setup(self.GLED,GPIO.OUT,initial=GPIO.LOW)
         #  シリアス通信のセットアップ
         #　出力データのセットアップ
         self.mag_status_next = 1
@@ -36,15 +36,16 @@ class Sensor ():
 
     #　現在、窓の開閉
     def getMagstatus(self):
-        return GPIO.input(MagPIN)
+        return GPIO.input(self.MagPIN)
 
     #　前回の換気時刻と時刻更新有無
     def getPreviousOpenWindow(self, magstatus):
         #　窓が開いた場合
-        if self.mag_status_next < magstatus:
+        if self.mag_status_next > magstatus:
             self.nowTime = datetime.datetime.now()
+            print(self.nowTime)
         #　窓が閉じた場合
-        elif self.mag_status_next > magstatus:
+        elif self.mag_status_next < magstatus:
             # 5分以上開いてた場合, 換気時刻更新
             if self.getBetweenTime(self.nowTime) >= self.min5Time:
                 self.PreviousTime = datetime.datetime.now()
@@ -53,15 +54,15 @@ class Sensor ():
         return self.PreviousTime
 
     #　換気推奨時間超過の有無
-    def getOpenWindow(self, NnmTime):
+    def getOpenWindow(self, NumTime):
         # 現在の換気推奨時間を超えていた場合,
-        result = self.getBetweenTime(self.PreviousTime) >= (self.hourTime/NumTime)
-        if result: 
+        result = self.getBetweenTime(self.PreviousTime) >= (self.hourTime/NumTime) and self.mag_status_next
+        if result:
                 self.setOpenLED(self.LED)
         else:
                 self.setCloseLED(self.LED)
         return result
-        
+
     #　現在時刻までの秒単位時間経過
     def getBetweenTime(self, laterTime):
         td = datetime.datetime.now() - laterTime
@@ -74,7 +75,7 @@ class Sensor ():
         return ppm
 
     # ppmの基準値超過の有無
-    def getCO2over(self, ppmstatus)
+    def getCO2over(self, ppmstatus):
         if ppmstatus < self.ppmOk:
             self.setOpenLED(self.GLED)
             self.setCloseLED(self.YLED)
@@ -90,19 +91,19 @@ class Sensor ():
 
     #　LEDを光らす
     def setOpenLED(self, LED_PIN):
-        self.GPIO.output(LED_PIN,GPIO.HIGH)
+        GPIO.output(LED_PIN,GPIO.HIGH)
     
     #　LEDを消す
     def setCloseLED(self,LED_PIN):
-         self.GPIO.output(LED_PIN,GPIO.LOW)
+         GPIO.output(LED_PIN,GPIO.LOW)
 
     #define a destroy function for clean up everything after the script finished
     def close(self):
-        self.GPIO.output(LED,GPIO.LOW)
-        self.GPIO.output(RLED,GPIO.LOW)
-        self.GPIO.output(YLED,GPIO.LOW)
-        self.GPIO.output(GLED,GPIO.LOW)
-        self.GPIO.cleanup()
+        GPIO.output(self.LED,GPIO.LOW)
+        GPIO.output(self.RLED,GPIO.LOW)
+        GPIO.output(self.YLED,GPIO.LOW)
+        GPIO.output(self.GLED,GPIO.LOW)
+        GPIO.cleanup()
 
 # if run this script directly ,do:
 if __name__ == '__main__':
@@ -110,10 +111,10 @@ if __name__ == '__main__':
     try:
         while True:
             print(s.getMagstatus())
-            print(s.getOpenWindow(s.getMagstatus()))
-            print(s.getPreviousOpenWindow(2))
-            print(s.getCO2over(s.getCO2Status))
-            time.sleep(0.5)
+            print(s.getPreviousOpenWindow(s.getMagstatus()))
+            print(s.getOpenWindow(2))
+            s.getCO2over(s.getCO2Status())
+            time.sleep(1)
     #when 'Ctrl+C' is pressed,child program destroy() will be executed.
     except KeyboardInterrupt:
         s.close()
