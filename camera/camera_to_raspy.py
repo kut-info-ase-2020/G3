ae0b31626d9cef13830da2ca127f52b881d54981
mi3x <245114y@gs.kochi-tech.ac.jp>
2020-08-31 00:48:05 +0900

    delete Japanese comments.

--- camera/camera_to_raspy.py
+++ camera/camera_to_raspy.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 import cv2
 
-cap = cv2.VideoCapture(0) # VideoCaptureのインスタンスを作成する。(複数のカメラがあるときは引数で識別)
+cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cap.read()     # 1フレーム読み込む
+    ret, frame = cap.read()
 
     # 画像を表示する
     cv2.imshow('Image', frame)
 
-    k = cv2.waitKey(1)  #引数は待ち時間(ms)
-    if k == 27: #Esc入力時は終了
+    k = cv2.waitKey(1)
+    if k == 27:
         break
 
 # 終了処理
