43702d823920a3c2b664f24504dbccafb338431c
TanakaYaemi <245125e@gs.kochi-tech.ac.jp>
2020-09-11 16:35:41 +0900

    Fixes for PythonToRealsensetest

--- camera/PythonToRealsensetest.py
+++ camera/PythonToRealsensetest.py
@@ -2,7 +2,6 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 
-# ストリーム(IR/Color/Depth)の設定
 config = rs.config()
 
 config.enable_stream(rs.stream.infrared, 1, 640, 480, rs.format.y8, 30)
@@ -10,13 +9,11 @@ config.enable_stream(rs.stream.infrared, 2, 640, 480, rs.format.y8, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
-# ストリーミング開始
 pipeline = rs.pipeline()
 profile = pipeline.start(config)
 
 try:
     while True:
-        # フレーム待ち
         frames = pipeline.wait_for_frames()
         #IR１
         ir_frame1 = frames.get_infrared_frame(1)
@@ -34,10 +31,9 @@ try:
         depth_frame = frames.get_depth_frame()
         depth_image = np.asanyarray(depth_frame.get_data())
 
-        #depth imageをカラーマップに変換
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.08), cv2.COLORMAP_JET)
 
-        # 表示
+
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', depth_colormap)
 
@@ -46,5 +42,5 @@ try:
             break
 
 finally:
-    # ストリーミング停止
+
     pipeline.stop()
