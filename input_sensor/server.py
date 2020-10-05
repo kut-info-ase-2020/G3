a189114cfaa6d3c25376b38f776ec2473f8baa48
mi3x <245114y@gs.kochi-tech.ac.jp>
2020-10-05 18:58:38 +0900

    test_server4_sig.pyのプロセス部分をマルチプロセスの記述に変更し、プロセス終了時にシグナルが動くように変更

--- input_sensor/server.py
+++ input_sensor/server.py
@@ -10,32 +10,67 @@ import time
 import ast
 ### add pack signal ###
 import signal
-import time
 from multiprocessing import Process, Queue, Event, Pipe
 
 #pid_list=[]
+#サーバープログラムver4、ファイルを生成し、まとめて送れる様になった。
+url="https://script.google.com/macros/s/AKfycbzTu4MasOfwmuYoIvxCvXpV4cMOCF_vLgyf4SmsjUPz7UN7sjt7/exec"
+file_send_time = 30
+#start_time = 0
 
 ### signal ###
 def receive_signal(signum, stack):
-    print('receive', signum)
+    print('------ receive signum is :', signum)
+
+### time ###
+#def time_measure():
+#    time.time()
 
 ### process ###
 def connect_to_process(conn, addr):
     with conn:
+        start_time = time.time()
         while True:
-            data = conn.recv(1024)
-            
+            data = conn.recv(1024)  
             if not data:
                 break
             print('data: {}, addr: {}'.format(data, addr))
-            #print(os.getpid())
+            # change .bin -> .json
+            data = data.decode()
+            json_dict = json.loads(data)
+
+            if json_dict.get('id') == 1:
+                alarm_sound(json_dict)
+            else:
+                json_dict["Day"] = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f")
+                file_name = 'test' + str(json_dict.get('id')) + '.json'
+                append_json_to_file(json_dict, file_name)
+
+                if(time.time() - start_time ) > file_send_time:
+                    with open(file_name,'r') as f:
+                        json_list = json.load(f)
+                    #jsonファイル内のデータを送信
+                    response = requests.post(url, data=json.dumps(json_list))
+                    #Google sheetsからのレスポンス確認
+                    print('raspons ' + response.text)
+                    #ファイル削除によってログデータリセット
+                    os.remove(file_name)
+                    #再度、時間計測
+                    start_time = time.time()
             conn.sendall(b'ok') # returned client for 'data'
     #print("pid list is %s ." % pid_list)  
     print('closed')
+    conn.close()
 
-### sound ###
 
-### change file bin -> json ###
+### sound ### 
+def alarm_sound(json_dict): 
+    with open("./sample.json", "r") as f:
+        flg_data = json.load(f)
+        strnum = json.dumps(flg_data)
+        bindata = strnum.encode()
+
+### add writing to json file ###
 def append_json_to_file(data: dict, path_file: str) -> bool:
     with open(path_file, 'ab+') as f:
         f.seek(0,2)
@@ -54,12 +89,11 @@ signal.signal(signal.SIGTERM, receive_signal)
 
 ### main ###
 if __name__ == '__main__' :
-
+    ### socket set ###
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(('127.0.0.1', 50007)) # IPaddr = server address. 
         s.listen(1)
 
-        # socket loop
         while True:
             print('wait connection...')
             conn, addr = s.accept()
